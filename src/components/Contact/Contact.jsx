f75cbcc01ad17613c8ee6907d38ea61f9cae1289
Aniket <122380583+Aniket0-7@users.noreply.github.com>
2023-04-01 03:13:26 +0530

    firebase setup

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,27 +2,67 @@ import "./Contact.css";
 import call from "../../img/call.png";
 import email from "../../img/email.png";
 import address from "../../img/address.png";
-import emailjs from '@emailjs/browser';
-import { useRef, useState } from "react";
-
-const Contact = () =>{
-
-    const formref = useRef()
-    const[done,setdone]=useState(false);
+import { useState } from "react";
 
 
-    const handelSumbit = (e) =>{
-        e.preventDefault();
-        emailjs.sendForm('service_zksz3ur', 'template_5jymga7', formref.current, 'Cnhg6jqe7tY-n09dS')
-        .then((result) => {
-            console.log(result.text);
-            setdone(true);
-        }, (error) => {
-            console.log(error.text);
-        });
+const Contact = () => {
+
+    const [user, setuser] = useState({
+        name: "",
+        subject: "",
+        email: "",
+        message: "",
+    });
+
+    let name, value;
+
+    const getuserdata = (event) => {
+        name = event.target.name;
+        value = event.target.value;
+
+        setuser({ ...user, [name]: value })
     };
-    
-     
+
+    const postdata = async (e) => {
+        e.preventDefault();
+
+        const { name, subject, email, message } = user;
+
+        if ((name && subject && email && message)) {
+            const res = await fetch("https://my-portfolio-6bfbc-default-rtdb.firebaseio.com/myportfoliodataform.json ",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name,
+                    subject,
+                    email,
+                    message,
+                })
+            }
+
+        );
+
+        if (res) {
+            setuser({
+                name: "",
+                subject: "",
+                email: "",
+                message: "",
+            });
+            alert("Your Response Submitted Sucessfully")
+        }
+        }else{
+
+            alert("Please Fil all the data ")
+
+        }
+
+        
+
+    };
 
     return (
         <div className="c" id="contact">
@@ -32,30 +72,58 @@ const Contact = () =>{
                     <h1 className="c-title">Let's discuss your project</h1>
                     <div className="c-info">
                         <div className="c-info-item">
-                            <img src={call} alt=""  className="c-icon"/>
+                            <img src={call} alt="" className="c-icon" />
                             +91 8144434917
                         </div>
                         <div className="c-info-item">
-                            <img src={email} alt=""  className="c-icon"/>
+                            <img src={email} alt="" className="c-icon" />
                             aniker207@gmail.com
                         </div>
                         <div className="c-info-item">
-                            <img src={address} alt=""  className="c-icon"/>
+                            <img src={address} alt="" className="c-icon" />
                             Sambalpur , Odisha
                         </div>
                     </div>
                 </div>
                 <div className="c-right">
                     <p className="c-desc">
-                    <b>What's your story?</b> Get in touch. Always available for freelancing if the right project comes along me.
+                        <b>What's your story?</b> Get in touch. Always available for freelancing if the right project comes along me.
                     </p>
-                    <form ref={formref}  onSubmit={handelSumbit} >
-                        <input type="text" placeholder="Name" name="user_name"  />
-                        <input type="text" placeholder="Subject" name="user_subject"  />
-                        <input type="text" placeholder="Email" name="user_email"  />
-                        <textarea rows="5" placeholder="Message" name="message" />
-                        <button>Submit</button>
-                        {done && "Thank you..."}
+                    <form method="POST" >
+                        <input type="text"
+                            value={user.name}
+                            onChange={getuserdata}
+                            placeholder="Name" name="name"
+                            autoComplete="off"
+                            required
+                        />
+
+                        <input type="text"
+                            value={user.subject}
+                            onChange={getuserdata}
+                            placeholder="Subject" name="subject"
+                            autoComplete="off"
+                            required
+                        />
+
+                        <input type="text"
+                            value={user.email}
+                            onChange={getuserdata}
+                            placeholder="Email" name="email"
+                            autoComplete="off"
+                            required
+                        />
+
+                        <textarea rows="5"
+                            value={user.message}
+                            onChange={getuserdata}
+                            placeholder="Message" name="message"
+                            autoComplete="off"
+                            required
+                        />
+
+                        <button onClick={postdata} >Submit</button>
+
                     </form>
                 </div>
             </div>
